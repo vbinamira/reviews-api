8139019f1e1a3e8c6f62f0f991de192b62a32e49
DSPCORP\vbinamira <vbinamira@davidsaxe.com>
2017-05-17 09:16:12 -0700

    Fix Dev Config

--- includes/api.php
+++ includes/api.php
@@ -6,7 +6,7 @@
 			$curl = curl_init();
 
 			curl_setopt_array($curl, array(
-			  CURLOPT_URL => "https://api.yelp.com/v3/businesses/4zD8uwQd1RCbpN1l0fJ64A/reviews",
+			  CURLOPT_URL => "https://api.yelp.com/v3/businesses/<businessID>/reviews",
 			  CURLOPT_RETURNTRANSFER => true,
 			  CURLOPT_ENCODING => "",
 			  CURLOPT_MAXREDIRS => 10,
@@ -14,7 +14,7 @@
 			  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
 			  CURLOPT_CUSTOMREQUEST => "GET",
 			  CURLOPT_HTTPHEADER => array(
-			    "authorization: Bearer 0qnAxRvlGraHJ7CxhOiuLwRlV7fAZvr7hOD5Gj9eFGaUvIjlivOT7hsp0YVgaadC8kQt79yz1i76A1uIP0uaZS6Avc065tbRuwJu85o8mDYcD7OEaT1Q_Je2h6ewWHYx",
+			    "authorization: Bearer <your_api_key>",
 			    "cache-control: no-cache",
 			    "postman-token: 00f2b7eb-a8b0-99a2-14a6-c094c46ea5f4"
 			  ),
